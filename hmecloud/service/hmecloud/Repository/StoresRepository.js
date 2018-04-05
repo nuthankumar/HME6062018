3123364a8531e12788fbe7211b6ef76dbe3e0a7b
NOUSINFO\jaffersr <jaffersr@nousinfo.com>
2018-04-05 18:49:41 +0530

    coding standards

--- hmecloud/service/hmecloud/Repository/StoresRepository.js
+++ hmecloud/service/hmecloud/Repository/StoresRepository.js
@@ -1,7 +1,4 @@
 const db = require('../DataBaseConnection/Configuration')
-const dateUtils = require('../Common/DateUtils')
-const HashMap = require('hashmap')
-const dateFormat = require('dateformat')
 
 const defaultFromTime = '00:00:00'
 const defaultEndTime = '23:59:59'
@@ -66,10 +63,7 @@ const timeMeasure = (callback) => {
 }
 
 const getRawCarDataReport = (input, callback) => {
-  const rawCarDataList = []
   const output = {}
-  const rawCarData = {}
-  const departTimeStampMap = new HashMap()
   let fromDateTime
   let toDateTime
   if (input.ReportTemplate_From_Time) {
@@ -89,51 +83,7 @@ const getRawCarDataReport = (input, callback) => {
   db.query(Query, {
     type: db.QueryTypes.SELECT
   }).then(result => {
-    const len = result.length
-    const storeData = result[len - 2]
-    const dayPartData = result[len - 1]
-    rawCarData.store = storeData.Store_Name
-    rawCarData.description = storeData.Brand_Name
-    rawCarData.startTime = input.ReportTemplate_From_Date
-    rawCarData.stopTime = input.ReportTemplate_To_Date
-    rawCarData.printDate = dateFormat(new Date(), 'isoDate')
-    rawCarData.printTime = dateFormat(new Date(), 'shortTime')
-    result.forEach(item => {
-      let rawCarTempId = item.RawDataID
-      if (rawCarTempId && !departTimeStampMap.has(rawCarTempId)) {
-        let departTimeStampList = result.filter(function (obj) {
-          return obj.RawDataID === rawCarTempId
-        })
-        let tempRawCarData = departTimeStampList[0]
-        const rawCarDataObj = {}
-        rawCarDataObj.departureTime = tempRawCarData.DepartTimeStamp
-        rawCarDataObj.eventName = tempRawCarData.CarRecordDataType_Name
-        rawCarDataObj.carsInQueue = tempRawCarData.CarsInQueue
-        rawCarData.dayPart = 'DP' + tempRawCarData.Daypart_ID + dateUtils.dayPartTime(tempRawCarData.Daypart_ID, len, dayPartData.StartTime, dayPartData.EndTime)
-
-        for (let i = 0; i < departTimeStampList.length; i++) {
-          let tempEventDetails = departTimeStampList[i]
-          if (tempEventDetails.EventType_Name.includes('Menu Board')) {
-            rawCarDataObj.menu = dateUtils.msFormat(tempEventDetails.DetectorTime, input.ReportTemplate_Format)
-          } else if (tempEventDetails.EventType_Name.includes('Lane Queue')) {
-            rawCarDataObj.laneQueue = dateUtils.msFormat(tempEventDetails.DetectorTime, input.ReportTemplate_Format)
-          } else if (tempEventDetails.EventType_Name.includes('Lane Total')) {
-            rawCarDataObj.laneTotal = dateUtils.msFormat(tempEventDetails.DetectorTime, input.ReportTemplate_Format)
-          } else if (tempEventDetails.EventType_Name.includes('Service')) {
-            rawCarDataObj.service = dateUtils.msFormat(tempEventDetails.DetectorTime, input.ReportTemplate_Format)
-          } else if (tempEventDetails.EventType_Name.includes('Greet')) {
-            rawCarDataObj.greet = dateUtils.msFormat(tempEventDetails.DetectorTime, input.ReportTemplate_Format)
-          }
-        }
-        rawCarDataList.push(rawCarDataObj)
-        departTimeStampMap.set(rawCarTempId, rawCarTempId)
-      }
-    })
-    rawCarData.rawCarData = rawCarDataList
-
-    output.data = rawCarData
-    output.status = true
-    callback(output)
+    callback(result)
   }).catch(error => {
     const output = {
       data: error,
