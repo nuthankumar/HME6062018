f0991c35a13715e685f527b91fcaf6103275d3a4
Swathikumar <swathik@digitalinfuzion.com>
2018-04-06 01:50:13 +0530

    Changes for data clean up

--- hmecloud/service/hmecloud/Common/DataBaseQueries.js
+++ hmecloud/service/hmecloud/Common/DataBaseQueries.js
@@ -1,10 +1,11 @@
 
 const sqlQueries = {
-  'GroupHierarchy': {
-    'getgroupDetails': 'exec [usp_GetGroupDetailsByGroupId] @GroupId = :groupId',
-    'getAllAvailableGroupsAndStores': 'exec [usp_GetAllAvailableGroupsAndStores] @AccountId = :accountId',
-    'getGroupHierarchy': 'exec [dbo].[usp_GetGroupHierarchy]  @AccountId= :accountId'
-  },
+   'GroupHierarchy': {
+        'getgroupDetails': 'exec [usp_GetGroupDetailsByGroupId] @GroupId = :groupId',
+        'getAllAvailableGroupsAndStores': 'exec [usp_GetAllAvailableGroupsAndStores] @AccountId = :accountId',
+        'getGroupHierarchy': 'exec [dbo].[usp_GetGroupHierarchy]  @AccountId= :accountId',
+        'deleteGroupByGroupId': 'exec usp_DeleteGroupByGroupId @GroupId= :groupId'
+   },
   'ReportTemplates': {
     'createReportTemplate': 'INSERT INTO dbo].[ReportTemplates] AccountId ,Stores,TimeMeasure,FromDate,ToDate ,OpenTime ,CloseTime,Type,Open,Close,Include,Format,TemplateName,CreatedBy,UpdatedBy,CreatedDateTime,UpdatedDateTime VALUES :AccountId,:Stores:TimeMeasure,:FromDate,:ToDate,:OpenTime,:CloseTime,:Type,:Open,:Close, :Include,:Format,:TemplateName,:CreatedBy,:UpdatedBy, :CreatedDateTime,:UpdatedDateTime',
     'getAllReportsTemplates': 'exec [dbo].[GetReportTemplates] @AccountId =:AccountId, @CreatedBy =:CreatedBy',
