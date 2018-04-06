5489bd7fe65502c0965a3b6ef23d9210f7090f7b
Swathikumar <swathik@digitalinfuzion.com>
2018-04-06 13:59:37 +0530

    Json validator enabled

--- hmecloud/service/hmecloud/Common/DataBaseQueries.js
+++ hmecloud/service/hmecloud/Common/DataBaseQueries.js
@@ -4,10 +4,11 @@ const sqlQueries = {
     'getgroupDetails': 'exec [usp_GetGroupDetailsByGroupId] @GroupId = :groupId',
     'getAllAvailableGroupsAndStores': 'exec [usp_GetAllAvailableGroupsAndStores] @AccountId = :accountId',
     'getGroupHierarchy': 'exec [dbo].[usp_GetGroupHierarchy]  @AccountId= :accountId',
-    'deleteGroupByGroupId': 'exec usp_DeleteGroupByGroupId @GroupId= :groupId'
+    'deleteGroupByGroupId': 'exec usp_DeleteGroupByGroupId @GroupId= :groupId',
+    'createGroup': 'exec usp_CreateGroup :groupName, :description, :accountId, :userName, :groups, :stores'
   },
   'ReportTemplates': {
-    'createReportTemplate': '[dbo].[usp_InsertReportTemplate] @AccountId  =:AccountId  ,@Stores = :Stores,@TimeMeasure = :TimeMeasure ,@FromDate =:FromDate,@ToDate =:FromDate ,@OpenTime=:OpenTime ,@CloseTime =:CloseTime,@Type=:Type,@Open=:Open, @Close=:Close,@Include=:Include,@Format=:Format,@TemplateName=:TemplateName,@CreatedBy=:CreatedBy,@UpdatedBy=:UpdatedBy,@CreatedDateTime=:CreatedDateTime, @UpdatedDateTime=:UpdatedDateTime',
+    'createReportTemplate': 'exec [dbo].[usp_InsertReportTemplate] @AccountId  =:AccountId  ,@Stores = :Stores,@TimeMeasure = :TimeMeasure ,@FromDate =:FromDate,@ToDate =:FromDate ,@OpenTime=:OpenTime ,@CloseTime =:CloseTime,@Type=:Type,@Open=:Open, @Close=:Close,@Include=:Include,@Format=:Format,@TemplateName=:TemplateName,@CreatedBy=:CreatedBy,@UpdatedBy=:UpdatedBy,@CreatedDateTime=:CreatedDateTime, @UpdatedDateTime=:UpdatedDateTime',
     'getAllReportsTemplates': 'exec [dbo].[usp_GetReportTemplates] @AccountId =:AccountId, @CreatedBy =:CreatedBy',
     'getReportsTemplate': 'exec [dbo].[usp_GetReportTemplateByID]  @Id = :id',
     'deleteTemplate': 'exec [dbo].[usp_DeleteReportTemplate]  @Id = :id'
