d38f0cdc3b70e2b7cde93c69fcd99d28688d10a0
NOUSINFO\shashankdr <shashankdr@nousinfo.com>
2018-04-15 12:32:44 +0530

    Changes for group hierarchy edit and summary reports css changes

--- hmecloud/client/src/components/Group/ReportGroupHierarchy.js
+++ hmecloud/client/src/components/Group/ReportGroupHierarchy.js
@@ -45,7 +45,10 @@ export default class ReportGroupHierarchy extends React.Component {
     this.state.editGroup = true
     this.state.groupId = node.node.props.eventKey
     this.setState(this.state)
-    this.props.history.push('/groups', this.state)
+    if(node.node.props.type === 'group'){
+      this.props.history.push('/groups', this.state)
+    }
+
     console.log(node.node.props.eventKey)
   }
   render () {
@@ -55,7 +58,7 @@ export default class ReportGroupHierarchy extends React.Component {
         // item.Children = [];
         if (item.Children && item.Children.length) {
           return <TreeNode className='treeNode' title={item.Name}
-            key={item.Id}>{loop(item.Children)}</TreeNode>
+            key={item.Id} type={item.Type}>{loop(item.Children)}</TreeNode>
         }
         return <TreeNode className='treeNode' title={item.Name} key={item.Id}/>
       })
