ac5b7abd4170b1bf53b46a87f807cca472bfc6f7
NOUSINFO\jaffersr <jaffersr@nousinfo.com>
2018-04-19 00:07:44 +0530

    AuthenticationController.js file update with correct version

--- hmecloud/service/hmecloud/Controllers/AuthenticationController.js
+++ hmecloud/service/hmecloud/Controllers/AuthenticationController.js
@@ -4,27 +4,25 @@ const aad = require('azure-ad-jwt')
 
 function verifyToken (request, response, next) {
   const authorization = request.headers['authorization']
-  /*
   if (!authorization) {
     return response.status(403).send({
-      auth: true,
+      auth: false,
       message: 'authorization header is missing.'
     })
   }
-*/
-  /*
+
   if (authorization) {
-    var bearer = authorization.split(" ");
-    var jwtToken = bearer[1];
+    var bearer = authorization.split(' ')
+    var jwtToken = bearer[1]
 
     if (!jwtToken) {
       return response.status(403).send({
-        auth: true,
+        auth: false,
         message: 'token is missing.'
       })
     }
     /* AzureAD token verification */
-  /*
+    /*
     if (jwtToken) {
       aad.verify(jwtToken, null, function (err, result) {
         if (result) {
@@ -34,32 +32,31 @@ function verifyToken (request, response, next) {
         }
       });
     }
-
     */
-  // let encodeToken = Buffer.from(jwtToken, 'base64')
-  // let token = encodeToken.toString('ascii')
-  // jwt.verify(jwtToken, config.secret, function (err, decoded) {
-  //  console.log('err: ', err)
+    // let encodeToken = Buffer.from(jwtToken, 'base64')
+    // let token = encodeToken.toString('ascii')
+    jwt.verify(jwtToken, config.secret, function (err, decoded) {
+      console.log('err: ', err)
 
-  //  if (err) {
-  //    return response
-  //      .status(500)
-  //      .send({
-  //        auth: false,
-  //        message: 'Failed to authenticate token.'
-  //      })
-  //  }
+      if (err) {
+        return response
+          .status(500)
+          .send({
+            auth: false,
+            message: 'Failed to authenticate token.'
+          })
+      }
 
-  request.userUid = '68LKBP85C1SKH1FI3M7X40CJHKGU07FZ'
-  request.UserEmail = 'jaffersr@nousinfo.com' // decoded.User_EmailAddress
-  request.UserName = 'Hme Admin' // `${decoded.User_LastName}, ${decoded.User_FirstName}`
-  // request.Role = decoded.Role
-  request.AccountId = 100 // decoded.User_OwnerAccount_ID
-  // request.UserPreferenceValue = decoded.UserPreferencesValue
-  next()
-  // })
-}
-/*
+      request.userUid = decoded.User_UID
+      request.UserEmail = decoded.User_EmailAddress
+      request.UserName = `${decoded.User_LastName}, ${decoded.User_FirstName}`
+      // request.Role = decoded.Role
+      request.AccountId = decoded.User_OwnerAccount_ID
+      // request.UserPreferenceValue = decoded.UserPreferencesValue
+      next()
+    })
+  }
+  /*
   // check header or url parameters or post parameters for token
   const jwtToken = request.headers['x-access-token']
 
@@ -91,6 +88,6 @@ function verifyToken (request, response, next) {
     next()
   })
   */
-// }
+}
 
 module.exports = verifyToken
