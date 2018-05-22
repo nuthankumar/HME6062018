e74f6525b30776cfa8fec3fcdcf75a59ab6b1515
NOUSINFO\shashankdr <shashankdr@nousinfo.com>
2018-05-22 16:42:06 +0530

    Changes for email id in reports and store number addition in multi store

--- hmecloud/client/src/components/Alerts/Email.js
+++ hmecloud/client/src/components/Alerts/Email.js
@@ -2,8 +2,10 @@ import React from 'react'
 import "../Alerts/Alerts.css";
 import Footer from "../Footer/Footer";
 import HmeHeader from "../Header/HmeHeader";
+import { Config } from '../../Config'
 import t from '../Language/language'
 import * as languageSettings from '../Language/languageSettings'
+import AuthenticationService from '../Security/AuthenticationService'
 
 export default class EmailAlert extends React.Component {
     constructor(props) {
@@ -12,16 +14,18 @@ export default class EmailAlert extends React.Component {
             currentLanguage: languageSettings.getCurrentLanguage(),
             emailId: this.props.history.location.state
         }
-       
+        this.authService = new AuthenticationService(Config.authBaseUrl)
     }
     render() {
         const language = this.state.currentLanguage
+        let user = this.authService.isMasquerade() ? this.authService.getAdminProfile() : this.authService.getProfile();
+        let emailId =  user.User_EmailAddress 
         return (
             <section>
                     <div id="Content">
                             <div class="forms clear">
                         <div class="appr">
-                            <h4>{t[language].pleasecheckemailreport} {this.state.emailId}.</h4>
+                            <h4>{t[language].pleasecheckemailreport} {emailId}.</h4>
                                 </div>
                             </div>
                     </div>
