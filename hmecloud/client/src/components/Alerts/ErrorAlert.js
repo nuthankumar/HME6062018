240a019029c178674159b979abf88d1b845b9a6b
Nandish L. Majjagi (BLR GSS) <nandishlm@nousinfo.com>
2018-05-24 18:00:41 +0530

    error  messages list

--- hmecloud/client/src/components/Alerts/ErrorAlert.js
+++ hmecloud/client/src/components/Alerts/ErrorAlert.js
@@ -1,17 +1,60 @@
 import React from 'react'
+const _ = require("underscore");
 
 export default class ErrorAlert extends React.Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {
+        isMultiple: false
+    }
+
+    
+//    if (this.props.errorMessage && this.props.errors){
+//     if (_.contains(this.props.errors, this.props.errorMessage))
+//     {
+//        this.state.isMultiple = true;
+//     }
+//  }
+
+    }
+  
+  
+  
   render () {
+    if (this.props.errorMessage && !this.props.errors){
     if (this.props.errorMessage !== '' && this.props.errorMessage !== undefined && this.props.errorMessage !== null) {
       return (
         <div className='alert alert-danger'>
-          {this.props.errorMessage}
+          <ul>
+             <li>{this.props.errorMessage}</li>
+          </ul>
         </div>
       )
-    } else {
+    }
+  }
+    else if (this.props.errors) {
+      return (
+        <div className='alert alert-danger errors'>
+          <ul>
+          {this.renderErrors(this.props.errors)}
+          </ul>
+        </div>
+      )
+    }
+    else {
       return (
         <div />
       )
     }
   }
+
+  renderErrors(errors) {
+     if (errors) {
+        let errorList  = errors.map(function (error, index) {
+                 return (<li key={index}>{error}</li>)
+         });
+        return errorList;
+    }
+}
 }
