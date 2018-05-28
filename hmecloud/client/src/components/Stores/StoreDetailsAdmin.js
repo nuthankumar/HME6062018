7bbba62f3bcee1fe62ac0c1b2e93f570b3d2ba88
Nandish L. Majjagi (BLR GSS) <nandishlm@nousinfo.com>
2018-05-28 14:47:15 +0530

    store Admin and client reverted to previous check-in

--- hmecloud/client/src/components/Stores/StoreDetailsAdmin.js
+++ hmecloud/client/src/components/Stores/StoreDetailsAdmin.js
@@ -1,161 +1,150 @@
 import React, { Component } from 'react'
 import './Stores.css'
-import Pagination from '../Common/Pagination'
-// import t from '../Language/language'
-// import * as languageSettings from '../Language/languageSettings'
-// import { connect } from 'react-redux';
-// import { bindActionCreators } from 'redux';
-import { ButtonToolbar, DropdownButton, MenuItem } from 'react-bootstrap'
-const Online = require('../../images/connection_online.png')
-const Offline = require('../../images/connection_offline.png')
 
-// export default class BookList extends Component {
-class StoreDetail extends Component { // ensure you dont export component directly
-  constructor (props) {
-    super(props)
-    this.state = {
-      showStores: this.props.showStores,
-      stores: {},
-      pageSize: 10,
-      offset: 0,
-      data: [],
-      recordCount: 50
+import t from '../Language/language'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as storesFunctions from "../../actions/stores"
+const Online = require("../../images/connection_online.png")
+const Offline = require("../../images/connection_offline.png")
+
+class StoreDetail extends Component { //ensure you dont export component directly
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            showStores: this.props.showStores,
+            stores: {},
+            Ascending:true
+        }
+        this.renderDevices = this.renderDevices.bind(this)
     }
-    this.renderDevices = this.renderDevices.bind(this)
-    this.PageSizeValueDropdown = this.PageSizeValueDropdown.bind(this)
-    this.PageClicked = this.PageClicked.bind(this)
-    this.onSelectAlert = this.onSelectAlert.bind(this)
-    this.Search = this.Search.bind(this)
-  }
-  PageSizeValueDropdown (pageSize) {
-    this.setState({ pageSize })
-  }
-  PageClicked (value) {
-    if (value === null) {
-      return this.state.offset
-    } else {
-      this.setState({ offset: value })
-      return this.state.offset
+
+    componentWillMount () {
+       this.props.dispatch(storesFunctions.sortStores({ 'sortBy': 'Company_Name', 'sortType': 'DESC' }))
     }
-  }
-  onSelectAlert (eventKey) {
-    window.alert(`Alert from menu item.\neventKey: ${eventKey}`)
-  }
-  Search (e) {
-    if (e.key === 'Enter') {
-      console.log(e.target.value)
+
+
+    render () {
+        let sortParams = this.props.storesDetails.sortParams ? this.props.storesDetails.sortParams :  { 'sortBy': 'Brand_Name', 'sortType': 'DESC' }
+     
+        return (
+            <section className={"stores " + (this.state.showStores ? 'show' : 'hidden')}>
+                <div className="settings forms">
+                    <div class="settings_list">
+                        <div class="storesTable ctable">
+                            <table>
+                                <tbody><tr class="theader clear">
+                                    <th></th>
+                                    <th className={(sortParams.sortBy == 'Company_Name' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Company_Name' && sortParams.sortType == 'DESC')?'actcold':'')}><a><span id="Company_Name" onClick={this.sortStores.bind(this)}>Company Name</span></a></th>
+                                    <th className={(sortParams.sortBy == 'Brand_Name' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Brand_Name' && sortParams.sortType == 'DESC')?'actcold':'')}><a><span id="Brand_Name" onClick={this.sortStores.bind(this)}>Brand</span></a></th>
+                                    <th className={(sortParams.sortBy == 'Store_Number' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Store_Number' && sortParams.sortType == 'DESC')?'actcold':'')}><a> <span id="Store_Number" onClick={this.sortStores.bind(this)}>Store #</span></a></th>
+                                    <th className={(sortParams.sortBy == 'Store_Name' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Store_Name' && sortParams.sortType == 'DESC')?'actcold':'')}><a><span id="Store_Name" onClick={this.sortStores.bind(this)}>Store Name</span></a></th>
+                                    <th className={(sortParams.sortBy == 'Store_AddressLine1' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Store_AddressLine1' && sortParams.sortType == 'DESC')?'actcold':'')}><a> <span id="Store_AddressLine1" onClick={this.sortStores.bind(this)}>Store Address</span></a></th>
+                                    {/* style="width:100px;" */}
+                                    <th className={(sortParams.sortBy == 'Device_SerialNumber' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Device_SerialNumber' && sortParams.sortType == 'DESC')?'actcold':'')}><a> <span id="Device_SerialNumber" onClick={this.sortStores.bind(this)}>Serial #</span></a></th>
+                                    <th className={(sortParams.sortBy == 'Device_MainVersion' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Device_MainVersion' && sortParams.sortType == 'DESC')?'actcold':'')}><a> <span id="Device_MainVersion" onClick={this.sortStores.bind(this)}>Version</span></a></th>
+                                    <th className={(sortParams.sortBy == 'Subscription_Name' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Subscription_Name' && sortParams.sortType == 'DESC')?'actcold':'')}><a> <span id="Subscription_Name" onClick={this.sortStores.bind(this)}>Subscription</span></a></th>
+                                    <th className={(sortParams.sortBy == 'Device_IsActive' && sortParams.sortType == 'ASC')?'actcol':((sortParams.sortBy == 'Device_IsActive' && sortParams.sortType == 'DESC')?'actcold':'')}><a> <span id="Device_IsActive" onClick={this.sortStores.bind(this)}>Status</span></a></th>
+                                </tr>
+                                    {this.renderStores()}
+                                </tbody></table>
+                        </div>
+                        
+                    </div>
+                </div>
+            </section>
+        );
     }
-  }
-  render () {
-    return (
-      <section className={'stores ' + (this.state.showStores ? 'show' : 'hidden')}>
-        <div className='settings forms'>
-          <div class='settings_search clear storeHeight'>
-            <h3 class='clear fleft'>Stores</h3>
-          </div>
-          <div className='settings_plug clear storeHeight'>
 
-            <div className='search_pos'>
-              <div class='dropdown'>
-                <ButtonToolbar>
-                  <DropdownButton
-                    bsStyle='default'
-                    title=''
-                    id='dropdown-no-caret'
-                    onSelect={this.onSelectAlert}
-                  >
-                    <MenuItem eventKey='1'>Search all</MenuItem>
-                    <MenuItem eventKey='2'>Search brand</MenuItem>
-                    <MenuItem eventKey='3'>Search store #</MenuItem>
-                    <MenuItem eventKey='4'>Search store name</MenuItem>
-                    <MenuItem eventKey='5'>Search serial #</MenuItem>
-                    <MenuItem eventKey='6'>Search system version</MenuItem>
-                  </DropdownButton>
-                </ButtonToolbar>
-              </div>
-              <div class='search'>
-                <input type='text' className='searchBox' onKeyPress={this.Search} />
-                <span class='fa fa-search' />
-              </div>
-            </div>
-          </div>
-          <div class='settings_list'>
-            <div class='storesTable ctable'>
-              <table>
-                <tbody><tr class='theader clear'>
-                  <th />
-                  <th class='actcold'><a href='./?pg=SettingsStores&amp;sort=cnamed&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Company Name</a></th>
-                  <th><a href='./?pg=SettingsStores&amp;sort=bbrand&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Brand</a></th>
-                  <th><a href='./?pg=SettingsStores&amp;sort=sstord&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Store #</a></th>
-                  <th><a href='./?pg=SettingsStores&amp;sort=sstord&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Store Name</a></th>
-                  <th><a href='./?pg=SettingsStores&amp;sort=saddrd&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Store Address</a></th>
-                  {/* style="width:100px;" */}
-                  <th ><a href='./?pg=SettingsStores&amp;sort=dserd&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Serial #</a></th>
 
-                  {/* style="width:80px;" */}
-                  <th ><a href='./?pg=SettingsStores&amp;sort=sfirmd&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Version</a></th>
-                  <th><a href='./?pg=SettingsStores&amp;sort=subscd&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Subscription</a></th>
-                  <th><a href='./?pg=SettingsStores&amp;sort=cstatd&amp;uuid=&amp;path=Main&amp;page=1&amp;search=00000159&amp;per=10&amp;filter='>Status</a></th>
-                </tr>{this.renderStores()}
-                </tbody></table>
-              <Pagination
-                perPage={this.state.pageSize}
-                PageSizeValueChange={this.PageSizeValueDropdown}
-                offset={this.PageClicked}
-                recordCount={this.state.recordCount} />
-            </div>
+    renderDevices (devices) {
+        const { language } = this.state
+        let deviceRows = devices.map(function (device, index) {
+            return (
+                <tr key={index}>
+                    <td>{device.Device_SerialNumber}</td>
+                    <td>{device.Device_MainVersion}</td>
+                    <td>{device.Subscription_Name}</td>
+                    <td>
+                        <img src={Online} className={"cstat " + (device.Device_IsActive ? "" : "hidden")} alt="Device Online" />
+                        <img src={Offline} className={"cstat " + (!device.Device_IsActive ? "" : "hidden")} alt="Device Offline" />
+                        <span className="cstat">
+                            <a href="http://uat.hmedtcloud.com/?pg=SettingsDevices&amp;st=connect&amp;duid=BCA09B13-D63D-4A3E-87F9-E4A53103259B&amp;Session_UID=TQFAOEWY4QR3AH7COYC1M0JTH9VE7QDO&amp;User_UID=L7KRDI112UNTP8P4PTA9XINT5PUY0R0U&amp;IsLoggedIn=1">{device.Device_IsActive == 1 ? (device.Device_Name + ' ' + 'Online') : (device.Device_Name + ' ' + 'Offline')}</a>
+                        </span>
+                    </td>
+                </tr>
+            )
+        });
+        return deviceRows;
+    }
 
-          </div>
-        </div>
-      </section>
-    )
-  }
+    renderStores () {
+        const { stores, language } = this.state
+        let self = this
+        console.log(this.props.stores.adminStoreDetails);
+        let storeRows = this.props.stores.adminStoreDetails.storeList.map(function (store, index) {
+            return (
+                <tr class="tdata clear" key={index}>
+                    <td class="cdet store_checkbox"><input type="checkbox" name="edit_selected1" id="edit_selected1" onchange="addRemoveStores('F5EE2F75C7CE4725849E4B5626A888D9','1');" value="F5EE2F75C7CE4725849E4B5626A888D9" disabled="disabled" /></td>
+                    <td>{store.Company_Name} <br /><span class="edit_settings"><a href="#" class="opener view_details" onclick="passViewDetails('F5EE2F75C7CE4725849E4B5626A888D9','', '00000159', 'shomhme+serversplit19622@gmail.com', 'Wendy\'s', 'ZOOM');">View Details</a></span></td>
+                    <td>{store.Brand_Name}</td>
+                    <td>{store.Store_Number}</td>
+                    <td>{store.Store_Name}</td>
+                    <td>{store.Store_AddressLine1} </td>
+                    <td colspan="4">
+                        <table>
+                            <tbody>
+                                {self.renderDevices(store.Device_Details)}
+                            </tbody></table>
+                    </td>
+                </tr>
+            )
+        });
+        return storeRows;
+    }
 
-  renderDevices (devices) {
-    // const { language } = this.state
-    let deviceRows = devices.map(function (device, index) {
-      return (
-        <tr key={index}>
-          <td>{device.Device_SerialNumber}</td>
-          <td>{device.Device_MainVersion}</td>
-          <td>{device.Subscription_Name}</td>
-          <td>
-            <img src={Online} className={'cstat ' + (device.Device_IsActive ? '' : 'hidden')} alt='Device Online' />
-            <img src={Offline} className={'cstat ' + (!device.Device_IsActive ? '' : 'hidden')} alt='Device Offline' />
-            <span className='cstat'>
-              <a href='http://uat.hmedtcloud.com/?pg=SettingsDevices&amp;st=connect&amp;duid=BCA09B13-D63D-4A3E-87F9-E4A53103259B&amp;Session_UID=TQFAOEWY4QR3AH7COYC1M0JTH9VE7QDO&amp;User_UID=L7KRDI112UNTP8P4PTA9XINT5PUY0R0U&amp;IsLoggedIn=1'>{device.Device_IsActive == 1 ? (device.Device_Name + ' ' + 'Online') : (device.Device_Name + ' ' + 'Offline')}</a>
-            </span>
-          </td>
-        </tr>
-      )
-    })
-    return deviceRows
-  }
 
-  renderStores () {
-    // const { stores, language } = this.state
-    let self = this
-    console.log(this.props.stores.adminStoreDetails)
-    let storeRows = this.props.stores.adminStoreDetails.storeList.map(function (store, index) {
-      return (
-        <tr class='tdata clear' key={index}>
-          <td class='cdet store_checkbox'><input type='checkbox' name='edit_selected1' id='edit_selected1' onchange="addRemoveStores('F5EE2F75C7CE4725849E4B5626A888D9','1');" value='F5EE2F75C7CE4725849E4B5626A888D9' disabled='disabled' /></td>
-          <td>{store.Company_Name} <br /><span class='edit_settings'><a href='#' class='opener view_details' onclick="passViewDetails('F5EE2F75C7CE4725849E4B5626A888D9','', '00000159', 'shomhme+serversplit19622@gmail.com', 'Wendy\'s', 'ZOOM');">View Details</a></span></td>
-          <td>{store.Brand_Name}</td>
-          <td>{store.Store_Number}</td>
-          <td>{store.Store_Name}</td>
-          <td>{store.Store_AddressLine1} </td>
-          <td colspan='4'>
-            <table>
-              <tbody>
-                {self.renderDevices(store.Device_Details)}
-              </tbody></table>
-          </td>
-        </tr>
-      )
-    })
-    return storeRows
-  }
+    sortStores (e) {
+        this.state.Ascending = this.state.Ascending ? false : true;
+        this.setState(this.state);
+        let sortBy = e.target.id;
+        let sortType = this.state.Ascending ? 'DESC' : 'ASC'
+        let sortParams = { 'sortBy': sortBy, 'sortType': sortType }
+        this.props.dispatch(storesFunctions.sortStores(sortParams))
+    }
+
+
 }
 
-export default StoreDetail
+
+
+function mapStateToProps(state) {
+    return {
+        storesDetails: state.storeDetails,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ storesFunctions: storesFunctions }, dispatch);
+}
+
+export default connect(mapStateToProps)(StoreDetail)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- hmecloud/client/src/containers/StoreDetails.js
+++ hmecloud/client/src/containers/StoreDetails.js
@@ -1,63 +1,42 @@
 import React, { Component } from 'react'
-
-import t from '../components/Language/language'
 import * as languageSettings from '../components/Language/languageSettings'
 import StoreDetailsAdmin from '../components/Stores/StoreDetailsAdmin'
 import StoreDetailsClient from '../components/Stores/StoreDetailsClient'
-import { initStoresDetails , adminStoresDetails} from "../actions/stores";
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-//import { initViewStoresDetails} from "../actions/stores";
-
-
+import { initStoresDetails } from '../actions/stores'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 class StoreDetails extends Component {
   constructor (props) {
-    super(props);
+    super(props)
     this.state = {
       currentLanguage: languageSettings.getCurrentLanguage()
     }
- }
-componentWillMount() {   
-
-  this.props.initStoresDetails(); 
-  console.log(this.props);
- // this.props.dispatch(adminStoresDetails()); 
-}
-
+  }
+  componentWillMount () {
+    this.props.initStoresDetails()
+  }
 
   render () {
-    const language = this.state.currentLanguage
-    let displayData = this.props.systemStats
-     return (
-        <div>
-          <StoreDetailsClient showStores={true} stores={this.props.storesDetails}/>
-          <StoreDetailsAdmin showStores={false} stores={this.props.storesDetails}/>
-        </div>
-      )
+    return (
+      <div>
+        <StoreDetailsClient showStores={false} stores={this.props.storesDetails} />
+        <StoreDetailsAdmin showStores={true} stores={this.props.storesDetails} />
+      </div>
+    )
   }
-} 
+}
 
-function mapStateToProps(state) {
-    return {
-         books: state.books,
-         storesDetails: state.storeDetails,
-     }
- }
-
-//  function mapDispatchToProps(dispatch) {
-//   return bindActionCreators({
-//     initStoresDetails:initStoresDetails }, dispatch);
-//   } 
-
-
-  function matchDispatchToProps(dispatch) {
-    return bindActionCreators(
-      {
-        initStoresDetails:initStoresDetails
-      },    dispatch
-    );
-  } 
-
-
- export default connect(mapStateToProps, matchDispatchToProps)(StoreDetails)
-
+function mapStateToProps (state) {
+  return {
+    books: state.books,
+    storesDetails: state.storeDetails
+  }
+}
+function matchDispatchToProps (dispatch) {
+  return bindActionCreators(
+    {
+      initStoresDetails: initStoresDetails
+    }, dispatch
+  )
+}
+export default connect(mapStateToProps, matchDispatchToProps)(StoreDetails)
